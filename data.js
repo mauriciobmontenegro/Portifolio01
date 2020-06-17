e06fdbadf3ba5c31d3050cb628ab384dc53d4392
Mauricio Montenegro <m.b.mont17@gmail.com>
2020-06-17 17:16:43 -0300

    Videos ainda com modal

---
+++ data.js
@@ -0,0 +1,44 @@
+module.exports = [
+    {
+        id: "DDm0M_rZLJo",
+        title: "Clonando interface do Nubank (+ ANIMAÇÕES) com React Native",
+        duration: "73",
+        price: "Free",
+        featured: true,
+    },
+    {
+        id: "2nXsLpUCO20",
+        title: "Clonando interface do Instagram (+ SCROLL ANIMADO) com React Native",
+        duration: "58",
+        price: "Free",
+        featured: false,
+    },
+    {
+        id: "awRtgpRsdTQ",
+        title: "Recriando Pipefy do zero (com drag n' drop)",
+        duration: "71",
+        price: "Free",
+        featured: false,
+    },
+    {
+        id: "bg-U0xZwcRk",
+        title: "Clonando interface do Uber com React Native",
+        duration: "83",
+        price: "Free",
+        featured: true,
+    },
+    {
+        id: "nznujk__7sQ",
+        title: "Code Challenge: Criando app com Electron do zero!",
+        duration: "131",
+        price: "Free",
+        featured: false,
+    },
+    {
+        id: "hOsSDTxzrKQ",
+        title: "Criando app com Flutter",
+        duration: "73",
+        price: "Free",
+        featured: true,
+    },
+]
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+const videos = require("./data")
+
+server.use(express.static('public'))
+
+server.set("view engine", "njk")
+
+nunjucks.configure("views", {
+    express: server,
+    autoescape: false,
+    noCache: true,
+})
+
+server.get("/", function (req, res) {
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/57838986?s=460&u=7485c61f700dfb5f1d5d7bb89082993a6e2327f0&v=4",
+        name: "Mauricio Montenegro",
+        role: "Frontend Developer",
+        description: "Originally from Brazil, passionate about the tech scene with experiences in engineering, planning and management of projects/clients. Very dynamic and flexible due to living in many countries and being in touch with different cultures, characteristics that have helped me when managing a team.",
+        links: [
+            { name: "Github", url: "https://github.com/mmont17/" },
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/mauriciobmontenegro/" },
+            { name: "Twitter", url: "https://twitter.com/mmont17/" },
+            { name: "Instagram", url: "https://www.instagram.com/mauriciomontenegro/" },
+        ],
+    }
+    return res.render("about", { about: about })
+})
+
+server.get("/classes", function (req, res) {
+    return res.render("classes", { items: videos })
+})
+
+server.listen(5000, function () {
+    console.log("server is running")
+})
